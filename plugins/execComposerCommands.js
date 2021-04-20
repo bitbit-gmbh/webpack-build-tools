ef94c1fff82dc9e672131d54f35c0f833298e716
Rene Reimann <rene@die-pixler.de>
2021-04-20 07:04:58 +0200

    add ExecComposerCommands plugin, fix devmode handling

--- plugins/execComposerCommands.js
+++ plugins/execComposerCommands.js
@@ -2,20 +2,26 @@ const log = require('signale');
 
 class ExecComposerCommands {
 	constructor(options) {
-		if(options.devMode){
+		if(options.devMode === 'development'){
 			return;
 		}
+
+		this.devMode = options.devMode;
 	}
 
 	apply(compiler) {
 		compiler.hooks.environment.tap('Execute composer dumpautoload command', (
 		  stats
 		) => {
+			let mode = '';
+
 			if(!this.devMode) {
-				const execSync = require('child_process').execSync;
-				log.info('run composer dumpautoload');
-				execSync( 'composer dumpautoload --no-dev', { stdio: [ 0, 1, 2 ] } );
+				mode = ' --no-dev';
 			}
+
+			const execSync = require('child_process').execSync;
+			log.info('run composer dumpautoload'  + mode);
+			execSync( 'composer dumpautoload' + mode, { stdio: [ 0, 1, 2 ] } );
 		});
 	}
 }
